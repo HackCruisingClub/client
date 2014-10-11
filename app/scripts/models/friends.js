82d652ec89f0a0f11ffacf778fb1bc6d8ef00fa0
Vincent Lark <vincent.lark@gmail.com>
2014-10-11 21:05:48 +0200

    friends list

--- app/scripts/models/friends.js
+++ app/scripts/models/friends.js
@@ -2,8 +2,30 @@
 
 ABC.Models.Friends = Backbone.Collection.extend({
 
+  model: ABC.Models.User,
+
   fetch: function () {
-      return ABC.hoodie.store.findAll('friend');
+
+    var d = new $.Deferred();
+
+    d.resolve([
+      new ABC.Models.User({
+        username: 'clawfire',
+        name:     'Thibault Milan',
+        email:    'hello@thibaultmilan.com',
+        points:   125
+      }),
+      new ABC.Models.User({
+        username: 'hellgy',
+        name:     'Laurence',
+        email:    'laurence@redisdead.net',
+        points:   540
+      })
+    ]);
+
+    return d;
+
+    // return ABC.hoodie.store.findAll('friend');
   }
 
 });
--- app/scripts/views/friends.js
+++ app/scripts/views/friends.js
@@ -5,12 +5,48 @@ ABC.Views.Friends = Backbone.Marionette.View.extend({
   template: JST.friends,
 
   events: {
+  },
+
+  initialize: function () {
+
+  },
+
+  populate: function (friends, topusers) {
+
+    var insert = function (user) {
+      return [
+        '<li class="user">',
+          '<img src="http://avatars.io/email/', user.get('email'), '" class="profile-picture">',
+          '<span class="list-user-name">', user.niceName(), '</span>',
+          '<span class="list-user-points">', user.get('points'), '</span>',
+        '</li>'
+        ].join('');
+    };
+
+    var friendsList = this.$el.find('.friend-list');
+    friendsList.html(
+      _.map(friends, insert.bind(friendsList)).join('')
+    );
+
+    var topList = this.$el.find('.topuser-list');
+    topList.html(
+      _.map(topusers, insert.bind(topList)).join('')
+    );
 
   },
 
   render: function () {
 
-      return this.$el.html(this.template(ABC.app));
+    var self = this;
+
+    this.$el.html(this.template(ABC.app));
+
+    (new ABC.Models.Friends()).fetch()
+      .done(function (friends) {
+        self.populate(friends, []);
+      });
+
+    return this.$el;
 
   }
 
